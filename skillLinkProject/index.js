e3b9b59b9f4c36d043f22572091f303048011157
Yanapatt Pankaseam <yanapatt.pankaseam@m.swu.ac.th>
2025-03-22 11:25:36 +0700

    Return to Default

--- skillLinkProject/index.js
+++ skillLinkProject/index.js
@@ -42,7 +42,7 @@ app.post('/delete-oldest', authController.authenticate, taskController.deleteOld
 
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
